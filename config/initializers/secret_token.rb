34bc7045dad64e5a492acfb96e9af3428945282f
Tymoteusz Świstek <tswistek@sigma.inf.ug.edu.pl>
2012-12-04 12:36:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArchSer::Application.config.secret_token = '8e94a6bb048a2640b4044e4ee2a964f60552401ae37174d7c8fa5f5365c2c04d3a736a3a155dafdb7c74743251c2ef1a56db74d5ba3905df8c46327654073ba6'
