7d8505105a9fbcaf2a4f3132ec06565b7dcfa0b0
stymex <stymex@gmail.com>
2012-12-06 14:02:43 +0100

    drobne zmiany

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,28 @@
 class SessionsController < ApplicationController
-	def new
-	end
+  def new
+    
+  end
 
-	def create
-		auth_hash = request.env['omniauth.auth']
-		user = User.find_all_by_uid_and_nickname(auth_hash["uid"],auth_hash["info"]["nickname"]);
-    if user.count > 0 
+  def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.find_all_by_uid_and_nickname(auth_hash["uid"],auth_hash["info"]["nickname"]);
+    session["uid"]=auth_hash["uid"]
+    if user.count > 0
       render :text => "Welcome back! You have already signed up."
     else
       user = User.new :uid => auth_hash["uid"], :nickname=> auth_hash["info"]["nickname"], :email =>auth_hash["info"]["email"], :admin => false
       user.save
-      render :text => "Hi! You've signed up."
+      render :text => "You've signed up."
     end
-	end
+  end
 
-	def failure
-	end
-	
+  def failure
+    render :text => "Sorry, but you didn't allow access to our app!"
+  end
+
+  def destroy
+    session["uid"] = nil
+    render :text => "You've logged out!"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+
+  before_filter :auth
+  def auth
+    if !session["uid"].present?
+      redirect_to "/login"
+    end
+    
+    user = User.find_all_by_uid(session["uid"])
+
+    if !user.present?
+      redirect_to "/"
+    end
+  end
+
   # GET /users
   # GET /users.json
   def index
