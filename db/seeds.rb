f6ab7b2358215c386f7931b0a099ec8ed0d47ee0
stymex <stymex@gmail.com>
2012-12-19 11:49:36 +0100

    dodanie usera admina w seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(uid: 2510948, nickname: 'stymex', email: 'stymex@gmail.com', admin: true)
